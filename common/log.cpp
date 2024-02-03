73e82e57778564de2d972b128531984b7f6b712e
wen.jie <wenjie204811@gmail.com>
2024-02-03 19:18:04 +0800

    LogFormatter::init

--- common/log.cpp
+++ common/log.cpp
@@ -99,9 +99,80 @@ namespace common
     }
 
     void LogFormatter::init() {
-        std::vector<std::pair<std::string, int>> vec;
-        //unsigned int
-        size_t last_pos = 0;
+        //string format type
+        std::vector<std::tuple<std::string, std::string, int>> vec;
+        std::string nstr;
+        for(size_t i = 0; i < m_pattern.size(); i ++)
+        {
+            if (m_pattern[i] != '%')
+            {
+                nstr.append(1, m_pattern[i]);
+                continue;
+            }
+            if ((i + 1) < m_pattern.size() && m_pattern[i + 1] == '%')
+            {
+                nstr.append(1, '%');
+                continue;
+            }
+            size_t n = i+1;
+            size_t fmt_status = 0;
+            size_t fmt_begin = 0;
+            std::string str;
+            std::string fmt;
+            while (n < m_pattern.size())
+            {
+                if (isspace(m_pattern[1]))
+                    break;
+                if (fmt_status == 0)
+                {
+                    if (m_pattern[i] == '{')
+                    {
+                        str = m_pattern.substr(i + 1, n - 1 - 1);
+                        fmt_status = 1;
+                        fmt_begin = n;
+                        n++;
+                        continue;
+                    }
+                }
+                if (fmt_status == 1)
+                {
+                    if (m_pattern[n] == '}')
+                    {
+                        fmt = m_pattern.substr(fmt_begin + 1, n - fmt_begin -1);
+                        fmt_status = 2;
+                        break;
+                    }
+                }
+            }
+            if (fmt_status == 0)
+            {
+                if (!nstr.empty())
+                {
+                    vec.push_back(std::make_tuple(nstr, "", 0));
+                }
+                str = m_pattern.substr(i +1, n -i - 1);
+                vec.push_back(std::make_tuple(str, fmt, 1));
+                i = n;
+            } else if (fmt_status == 1)
+            {
+                std::cout << "pattern parse error: "
+                    << m_pattern << "-" << m_pattern.substr(i)
+                    << std::endl;
+                vec.push_back(std::make_tuple("<<pattern error>>", fmt, 0));
+            } else if(fmt_status == 2)
+            {
+                if (!nstr.empty())
+                {
+                    vec.push_back(std::make_tuple(nstr, "", 0));
+                }
+                vec.push_back(std::make_tuple(str, fmt, 1));
+                i = n;
+            }
+        }
 
+        if (!nstr.empty())
+        {
+            vec.push_back(std::make_tuple(nstr, "", 0));
+        }
     }
 }
