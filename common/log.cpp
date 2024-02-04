c3b771f8c3642edf4bcebe013398f31f48ab27fa
wen.jie <wenjie204811@gmail.com>
2024-02-04 13:50:01 +0800

    LogFormatter::init 修改

--- common/log.cpp
+++ common/log.cpp
@@ -144,6 +144,7 @@ namespace common
     Logger::Logger(const std::string& name) : m_name(name), m_level(LogLevel::INFO)
     {
         m_formatter.reset(new LogFormatter("%d [%p] %f:%l %m %n"));
+        //std::cout << m_name << std::endl;
     }
 
     void Logger::addAppender(LogAppender::ptr appender)
@@ -246,77 +247,77 @@ namespace common
     }
 
     void LogFormatter::init() {
-    //str, format, type
-    std::vector<std::tuple<std::string, std::string, int> > vec;
-    std::string nstr;
-    for(size_t i = 0; i < m_pattern.size(); ++i) {
-        if(m_pattern[i] != '%') {
-            nstr.append(1, m_pattern[i]);
-            continue;
-        }
-
-        if((i + 1) < m_pattern.size()) {
-            if(m_pattern[i + 1] == '%') {
-                nstr.append(1, '%');
+        //str, format, type
+        std::vector<std::tuple<std::string, std::string, int> > vec;
+        std::string nstr;
+        for(size_t i = 0; i < m_pattern.size(); ++i) {
+            if(m_pattern[i] != '%') {
+                nstr.append(1, m_pattern[i]);
                 continue;
             }
-        }
 
-        size_t n = i + 1;
-        int fmt_status = 0;
-        size_t fmt_begin = 0;
-
-        std::string str;
-        std::string fmt;
-        while(n < m_pattern.size()) {
-            if(!fmt_status && (!isalpha(m_pattern[n]) && m_pattern[n] != '{'
-                    && m_pattern[n] != '}')) {
-                str = m_pattern.substr(i + 1, n - i - 1);
-                break;
-            }
-            if(fmt_status == 0) {
-                if(m_pattern[n] == '{') {
-                    str = m_pattern.substr(i + 1, n - i - 1);
-                    //std::cout << "*" << str << std::endl;
-                    fmt_status = 1; //解析格式
-                    fmt_begin = n;
-                    ++n;
+            if((i + 1) < m_pattern.size()) {
+                if(m_pattern[i + 1] == '%') {
+                    nstr.append(1, '%');
                     continue;
                 }
-            } else if(fmt_status == 1) {
-                if(m_pattern[n] == '}') {
-                    fmt = m_pattern.substr(fmt_begin + 1, n - fmt_begin - 1);
-                    //std::cout << "#" << fmt << std::endl;
-                    fmt_status = 0;
-                    ++n;
+            }
+
+            size_t n = i + 1;
+            int fmt_status = 0;
+            size_t fmt_begin = 0;
+
+            std::string str;
+            std::string fmt;
+            while(n < m_pattern.size()) {
+                if(!fmt_status && (!isalpha(m_pattern[n]) && m_pattern[n] != '{'
+                        && m_pattern[n] != '}')) {
+                    str = m_pattern.substr(i + 1, n - i - 1);
                     break;
                 }
-            }
-            ++n;
-            if(n == m_pattern.size()) {
-                if(str.empty()) {
-                    str = m_pattern.substr(i + 1);
+                if(fmt_status == 0) {
+                    if(m_pattern[n] == '{') {
+                        str = m_pattern.substr(i + 1, n - i - 1);
+                        //std::cout << "*" << str << std::endl;
+                        fmt_status = 1; //解析格式
+                        fmt_begin = n;
+                        ++n;
+                        continue;
+                    }
+                } else if(fmt_status == 1) {
+                    if(m_pattern[n] == '}') {
+                        fmt = m_pattern.substr(fmt_begin + 1, n - fmt_begin - 1);
+                        //std::cout << "#" << fmt << std::endl;
+                        fmt_status = 0;
+                        ++n;
+                        break;
+                    }
+                }
+                ++n;
+                if(n == m_pattern.size()) {
+                    if(str.empty()) {
+                        str = m_pattern.substr(i + 1);
+                    }
                 }
             }
-        }
 
-        if(fmt_status == 0) {
-            if(!nstr.empty()) {
-                vec.push_back(std::make_tuple(nstr, std::string(), 0));
-                nstr.clear();
+            if(fmt_status == 0) {
+                if(!nstr.empty()) {
+                    vec.push_back(std::make_tuple(nstr, std::string(), 0));
+                    nstr.clear();
+                }
+                vec.push_back(std::make_tuple(str, fmt, 1));
+                i = n - 1;
+            } else if(fmt_status == 1) {
+                std::cout << "pattern parse error: " << m_pattern << " - " << m_pattern.substr(i) << std::endl;
+                m_error = true;
+                vec.push_back(std::make_tuple("<<pattern_error>>", fmt, 0));
             }
-            vec.push_back(std::make_tuple(str, fmt, 1));
-            i = n - 1;
-        } else if(fmt_status == 1) {
-            std::cout << "pattern parse error: " << m_pattern << " - " << m_pattern.substr(i) << std::endl;
-            m_error = true;
-            vec.push_back(std::make_tuple("<<pattern_error>>", fmt, 0));
         }
-    }
 
-    if(!nstr.empty()) {
-        vec.push_back(std::make_tuple(nstr, "", 0));
-    }
+        if(!nstr.empty()) {
+            vec.push_back(std::make_tuple(nstr, "", 0));
+        }
 
         //%m 消息体
         //%p level
